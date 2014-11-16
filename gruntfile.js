a7b546ec4ede447507f356ee0a54a71c9036c0f2
Tyler Beck <tyler@tylerbeck.com>
2014-11-16 17:21:14 -0500

    re-adding gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+ * Default Grunt Scaffolding
+ * Author: Copyright 2012-2014, Tyler Beck
+ * License: MIT
+ ***********************************************************************/
+
+var GruntStartup = require('grunt-startup');
+
+module.exports = new GruntStartup( {
+
+    loadTasks: true,
+
+    ignoreTasks: [
+        'grunt-sketch'
+    ],
+    taskPaths: [
+        'automation/grunt/tasks/'
+    ],
+    configPaths: [
+        './default-settings.json',
+        './local-settings.json',
+        'automation/grunt/configuration/']
+} );
